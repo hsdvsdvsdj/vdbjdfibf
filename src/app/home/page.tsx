bf54bf19c242f363cd2ff68e97003fea3b943131
thE-hAppY-CVAGA <akkbronza4@gmail.com>
2026-03-19 17:17:34 +0300

    1

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import RootLayoutComponent from "../../components/RootLayout";
+import Home from "../../components/Home";
+
+export default function Page() {
+  return (
+    <RootLayoutComponent>
+      <Home />
+    </RootLayoutComponent>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import RootLayoutComponent from "../components/RootLayout";
+import Landing from "../components/Landing";
+
+export default function Page() {
+  return (
+    <RootLayoutComponent>
+      <Landing />
+    </RootLayoutComponent>
+  );
+}
